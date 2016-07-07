e1631d12dd27c1593d58d79e7a699b14ea9f4ed3
bhagavan <bhagavan@auranetworks.in>
2016-07-07 21:09:54 +0530

    adding openstack setup related files

---
+++ docs/openstack/set-my-env.sh
@@ -0,0 +1,6 @@
+set -v
+cp /home/bhagavan/auradev/docs/openstack/stackenv ./.stackenv
+cp /home/bhagavan/auradev/docs/openstack/myopenrc ./
+cp "/home/bhagavan/auradev/docs/openstack/print-my-env.sh" ./
+# source myopenrc admin admin
+# sh "./openstack-print-env.sh"
