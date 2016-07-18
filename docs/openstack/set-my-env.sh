841e5f2edfd7d6282d93e2d670d444eec444f5b9
bhagavan <bhagavan@auranetworks.in>
2016-07-18 11:59:01 +0530

    modifying files

--- docs/openstack/set-my-env.sh
+++ docs/openstack/set-my-env.sh
@@ -2,6 +2,7 @@ set -v
 cd /home/bhagavan/devstack
 cp /home/bhagavan/auradev/docs/openstack/stackenv ./.stackenv
 cp /home/bhagavan/auradev/docs/openstack/myopenrc ./
-cp "/home/bhagavan/auradev/docs/openstack/print-my-env.sh" ./
+cp /home/bhagavan/auradev/docs/openstack/print-my-env.sh ./
+cd /home/bhagavan/devstack
 # source myopenrc admin admin
-# sh "./openstack-print-env.sh"
+# sh "./print-my-env.sh"
