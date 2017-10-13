23a9c3fd3ded4f551099db0ea22082897da0b87a
bhagavan <bhagavan@auranetworks.in>
2017-10-13 08:23:42 +0530

    Adding first version of R scripts

---
+++ scripts/R/03-utils/01-utils.R
@@ -0,0 +1,12 @@
+help(sqrt)
+example(sqrt)
+
+help(read.csv)
+example(read.csv)
+
+print (sum(1, 3, 5))
+
+dir()
+
+getwd()
+
---
+++ scripts/R/05-CSV-Handlling/01-csv-basics.R
@@ -0,0 +1,8 @@
+students <- read.csv(file="users-data.csv", head=TRUE, sep=",")
+print (students)
+summary(students)
+
+print (students$name)
+students$name
+print (students$fullname)
+names(students)
